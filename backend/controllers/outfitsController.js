f1d4c210fe999d2ea2869828e270a1c7bf7a12e1
hmz44 <126123010+hmz44@users.noreply.github.com>
2023-12-21 13:10:56 -0500

    outfits and images can be associated with one another

--- backend/controllers/outfitsController.js
+++ backend/controllers/outfitsController.js
@@ -1,1 +1,33 @@
-g
+const { Outfits, Images } = require('../models');
+
+const createOutfit = async (req, res, next) => {
+  try {
+    const { location, occasion, images } = req.body;
+
+    // Creates the outfit
+    const newOutfit = await Outfits.create({ location, occasion });
+
+    // If images exist, associate them with the outfit
+    if (images && images.length > 0) {
+      // async sends multiple requests
+      const imagePromises = images.map(async (image) => {
+        // Create images and associate them with outfit
+        return Images.create({ url: image, outfit_id: newOutfit.id });
+      });
+
+      await Promise.all(imagePromises);
+    }
+
+    res.status(201).json(newOutfit);
+
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in createOutfit controller: ${err}`,
+      status: 400,
+      message: { err: 'An error occurred while creating the outfit' },
+    });
+  }
+};
+
+module.exports = { createOutfit };
